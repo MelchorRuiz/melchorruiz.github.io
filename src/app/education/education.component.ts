32f50ff261cf9e14933e7501b547460411c9aad4
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-09-21 23:03:58 +0200

    Add education component

---
+++ src/app/education/education.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import schools from '../../data/schools';
+
+@Component({
+  selector: 'app-education',
+  standalone: true,
+  imports: [],
+  template: `
+    <h2 class="text-2xl font-semibold py-2">Educación</h2>
+    <div class="flex flex-col gap-2">
+      @for (school of schools; track school.id) {
+        <div class="border-[1px] border-black rounded-lg p-4">
+          <div class="flex gap-4">
+            <img class="size-20 rounded-full" src="{{ school.logo }}" alt="">
+            <div>
+              <h5 class="text-xl font-medium">{{ school.name }}</h5>
+              <p>{{ school.degree }} </p>
+              <p>{{ school.startDate }} - {{ school.endDate }}</p>
+            </div>
+          </div>
+          <ul class="flex flex-wrap gap-2 pt-4">
+            @for (skill of school.skills; track skill) {
+              <li class="border-[1px] border-black py-1 px-2 hover:bg-black hover:text-white">{{ skill }}</li>
+            }
+          </ul>
+        </div>
+      }
+    </div>
+  `,
+  styles: ``
+})
+export class EducationComponent {
+  schools = schools;
+}
