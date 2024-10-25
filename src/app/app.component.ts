cb146c6372493e8b87cabf68349ea2bc870cbbf7
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-10-25 14:51:59 +0200

    improve seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, LOCALE_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';
 import { HeroComponent } from "./hero/hero.component";
 import { AboutComponent } from "./about/about.component";
 import { ExperienceComponent } from "./experience/experience.component";
@@ -32,8 +33,34 @@ import { LocaleSelectorComponent } from "./locale-selector/locale-selector.compo
 export class AppComponent implements OnInit {
   currentYear: number = 0;
 
+  constructor(
+    private title: Title, 
+    private meta: Meta,
+    @Inject(LOCALE_ID) private locale: string
+  ) { }
+
   ngOnInit() {
     this.currentYear = new Date().getFullYear();
+    this.setMetaData();
+  }
+
+  setMetaData() {
+    this.title.setTitle('Melchor Ruiz');
+    this.meta.addTags([
+      { name: 'author', content: 'Melchor Ruiz' },
+      { name: 'description', content: 'Melchor Ruiz - Full Stack Developer' },
+      { name: 'keywords', content: 'Melchor Ruiz, Full Stack Developer, Angular, Node.js, JavaScript, TypeScript, HTML, CSS, SASS, Tailwind CSS, MySQL, MongoDB, Git, GitHub' },
+      { name: 'robots', content: 'index, follow' },
+      { rel: 'canonical', href: this.locale == 'fr' ? 'https://melchor-ruiz.fr/fr/' : 'https://melchor-ruiz.fr/es/' },
+      { rel: 'alternate', hreflang: 'es', href: 'https://melchor-ruiz.fr/es/' },
+      { rel: 'alternate', hreflang: 'fr', href: 'https://melchor-ruiz.fr/fr/' },
+      { rel: 'alternate', hreflang: 'x-default', href: 'https://melchor-ruiz.fr' },
+      { property: 'og:title', content: 'Melchor Ruiz' },
+      { property: 'og:description', content: 'Melchor Ruiz - Full Stack Developer' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: this.locale == 'fr' ? 'https://melchor-ruiz.fr/fr/' : 'https://melchor-ruiz.fr/es/' },
+      { property: 'og:image', content: this.locale == 'fr' ? 'https://melchor-ruiz.fr/fr/melchor.webp' : 'https://melchor-ruiz.fr/es/melchor.webp' }, // <-
+    ])
   }
   
 }
