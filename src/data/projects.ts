0c8b9885e1fd442b6e7e16da7531efc8cdbe8a90
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-10-25 11:45:15 +0200

    Add new projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,16 +16,48 @@ const projects_es: project[] = [
     github: 'https://github.com/MelchorRuiz/foro_electromovilidad_2024',
     link: 'https://foro-electromovilidad.igeco.mx/'
   },
+  {
+    id: 2,
+    name: 'Sicefa',
+    description: 'Sistema integral para la administración de múltiples farmacias pertenecientes a una misma empresa. Permite la gestión centralizada de sucursales, pedidos, envíos, ventas, empleados y clientes. Optimiza la logística, mejora el control de inventarios y facilita la toma de decisiones estratégicas empresariales.',
+    technologies: ['Javascript', 'mysql', 'java', 'docker', 'tomcat', 'jakarta'],
+    github: 'https://github.com/MelchorRuiz/Sicefa',
+    link: 'https://sicefa.onrender.com/'
+  },
+  {
+    id: 3,
+    name: 'CashPlash_Pro',
+    description: 'Solución integral diseñada para facilitar la administración de pequeños y medianos negocios. Los usuarios pueden agregar nuevos módulos al sistema a través de una interfaz dedicada, permitiendo la adaptación de la aplicación a las necesidades específicas de cada negocio.',
+    technologies: ['javascript', 'electron', 'html5', 'css3', 'mongodb', 'node.js'],
+    github: 'https://github.com/MelchorRuiz/CashPlash_Pro',
+    link: 'https://github.com/MelchorRuiz/CashPlash_Pro/releases/tag/v1.0.0'
+  },
 ]
 
 const projects_fr: project[] = [
   {
     id: 1,
-    name: 'Forum Électromobilité 2024',
+    name: 'Foro Electromovilidad 2024',
     description: 'Ce projet présente une page d\'atterrissage conçue pour le « Foro Electromovilidad 2024 », une conférence dédiée à l\'exploration des dernières tendances, innovations et développements dans le domaine de l\'électromobilité.',
     technologies: ['Astro', 'TailwindCSS', 'React', 'Node.js', 'Express', 'MySQL'],
     github: 'https://github.com/MelchorRuiz/foro_electromovilidad_2024',
     link: 'https://foro-electromovilidad.igeco.mx/'
+  },
+  {
+    id: 2,
+    name: 'Sicefa',
+    description: 'Système intégré pour la gestion de plusieurs pharmacies. Il permet la gestion centralisée des succursales, des commandes, des expéditions, des ventes, des employés et des clients. Il optimise la logistique, améliore le contrôle des stocks et facilite la prise de décisions stratégiques au sein de l\'entreprise.',
+    technologies: ['Javascript', 'mysql', 'java', 'docker', 'tomcat', 'jakarta'],
+    github: 'https://github.com/MelchorRuiz/Sicefa',
+    link: 'https://sicefa.onrender.com/'
+  },
+  {
+    id: 3,
+    name: 'CashPlash_Pro',
+    description: 'Solución intégrale conçue pour faciliter la gestion des petites et moyennes entreprises. Les utilisateurs peuvent ajouter de nouveaux modules au système via une interface dédiée, permettant ainsi d\'adapter l\'application aux besoins spécifiques de chaque entreprise.',
+    technologies: ['javascript', 'electron', 'html5', 'css3', 'mongodb', 'node.js'],
+    github: 'https://github.com/MelchorRuiz/CashPlash_Pro',
+    link: 'https://github.com/MelchorRuiz/CashPlash_Pro/releases/tag/v1.0.0'
   },
 ]
 
